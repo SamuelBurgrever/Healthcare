44bb6455264d43499986b29ccb6c343f3be21f5d
jonasezequiel <jonasmachado11@gmail.com>
2024-06-28 21:41:07 -0300

    adicionei a classe medico

--- src/main/java/com/ads/healthcare/entities/Medico.java
+++ src/main/java/com/ads/healthcare/entities/Medico.java
@@ -1,5 +1,53 @@
 package com.ads.healthcare.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Medico {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String nome;
+    private String crm;
+    private String telefone;
+
+    public Medico() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
 }
