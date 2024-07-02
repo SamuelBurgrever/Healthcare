0115b34fc14a25086dcf6c1c189147a5cccecbef
jonasezequiel <jonasmachado11@gmail.com>
2024-07-02 20:18:45 -0300

    Fiz os services das tabelas material, procedimento, medicamento

--- src/main/java/com/ads/healthcare/services/MaterialService.java
+++ src/main/java/com/ads/healthcare/services/MaterialService.java
@@ -1,5 +1,27 @@
 package com.ads.healthcare.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ads.healthcare.entities.Material;
+import com.ads.healthcare.repository.MaterialRepository;
+
+@Service
 public class MaterialService {
-    
+    @Autowired
+    MaterialRepository repository;
+
+    public List<Material> listar() {
+        List<Material> materiais = repository.findAll();
+        return materiais.stream().map(x -> new Material()).collect(Collectors.toList());
+
+    }
+
+    public String cadastro(Material material) {
+        repository.save(material);
+        return "Cadastrado com suceso!!!";
+    }
 }
--- src/main/java/com/ads/healthcare/services/ProcedimentoService.java
+++ src/main/java/com/ads/healthcare/services/ProcedimentoService.java
@@ -1,5 +1,27 @@
 package com.ads.healthcare.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ads.healthcare.entities.Procedimento;
+import com.ads.healthcare.repository.ProcedimentoRepository;
+
+@Service
 public class ProcedimentoService {
-    
+    @Autowired
+    ProcedimentoRepository repository;
+
+    public List<Procedimento> listar() {
+        List<Procedimento> procedimentos = repository.findAll();
+        return procedimentos.stream().map(x -> new Procedimento()).collect(Collectors.toList());
+
+    }
+
+    public String cadastro(Procedimento procedimento) {
+        repository.save(procedimento);
+        return "Cadastrado com suceso!!!";
+    }
 }
