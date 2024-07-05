fe0ffd64b6841319eedb73cc39c40ab05ec56b98
Jonas <jonasmachado11@gmail.com>
2024-07-04 21:35:07 -0300

    Parte final, incompleto

--- src/main/java/com/ads/healthcare/controllers/FaturamentoController.java
+++ src/main/java/com/ads/healthcare/controllers/FaturamentoController.java
@@ -1,5 +1,25 @@
 package com.ads.healthcare.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ads.healthcare.entities.Faturamento;
+import com.ads.healthcare.services.FaturamentoService;
+
+@RestController
+@RequestMapping("/faturamento")
 public class FaturamentoController {
-    
+
+    @Autowired
+    FaturamentoService service;
+
+
+    @PostMapping("/cadastro")
+    public ResponseEntity<String> cadastrar(Faturamento faturamento) {
+        return new ResponseEntity<>(service.cadastrar(faturamento), HttpStatus.OK);
+    }
 }
--- src/main/java/com/ads/healthcare/entities/Faturamento.java
+++ src/main/java/com/ads/healthcare/entities/Faturamento.java
@@ -17,4 +17,57 @@ public class Faturamento {
 
     @ManyToOne(cascade = CascadeType.REFRESH)
     private Material material_faturamento;
+
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    private Procedimento procedimento_faturamento;
+
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    private Prescricao prescricao_faturamento;
+
+    public Faturamento() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Material getMaterial_faturamento() {
+        return material_faturamento;
+    }
+
+    public void setMaterial_faturamento(Material material_faturamento) {
+        this.material_faturamento = material_faturamento;
+    }
+
+    public Procedimento getProcedimento_faturamento() {
+        return procedimento_faturamento;
+    }
+
+    public void setProcedimento_faturamento(Procedimento procedimento_faturamento) {
+        this.procedimento_faturamento = procedimento_faturamento;
+    }
+
+    public Prescricao getPrescricao_faturamento() {
+        return prescricao_faturamento;
+    }
+
+    public void setPrescricao_faturamento(Prescricao prescricao_faturamento) {
+        this.prescricao_faturamento = prescricao_faturamento;
+    }
+
+    
+
+    
 }
